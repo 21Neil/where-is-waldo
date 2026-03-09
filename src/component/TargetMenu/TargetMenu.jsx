b28c587d8b241cb32570fd3b2c5256f6ab3c5dde
21Neil <88711171+21Neil@users.noreply.github.com>
2026-03-09 16:59:11 +0800

    feat: add targetOnClick func

--- src/component/TargetMenu/TargetMenu.jsx
+++ src/component/TargetMenu/TargetMenu.jsx
@@ -1,17 +1,46 @@
 import styles from './TargetMenu.module.css';
 
-const TargetMenu = ({ ref, coord, visible }) => {
+const TargetMenu = ({ ref, coord, visible, cordRef, levelId }) => {
+  const targetOnClick = async name => {
+    const res = await fetch(
+      import.meta.env.VITE_API_BASE_URL + '/game/check-location',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          levelId,
+          x: cordRef.current.x,
+          y: cordRef.current.y,
+        }),
+      },
+    );
+
+    const result = await res.json();
+    console.log(result);
+  };
+
   return (
     <div
-      className={`${styles.container} ${ visible ? styles.visible : ''}`}
+      className={`${styles.container} ${visible ? styles.visible : ''}`}
       style={{ left: coord.x, top: coord.y }}
       ref={ref}
     >
       <ul>
-        <li><button>Waldo</button></li>
-        <li><button>Wanda</button></li>
-        <li><button>Wizard</button></li>
-        <li><button>Odlaw</button></li>
+        <li>
+          <button onClick={() => targetOnClick('Waldo')}>Waldo</button>
+        </li>
+        <li>
+          <button onClick={() => targetOnClick('Wanda')}>Wanda</button>
+        </li>
+        <li>
+          <button onClick={() => targetOnClick('Wizard')}>Wizard</button>
+        </li>
+        <li>
+          <button onClick={() => targetOnClick('Odlaw')}>Odlaw</button>
+        </li>
       </ul>
     </div>
   );
